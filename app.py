40c5d9547667db20f9de7749fcdcfb08548563a1
emipalma04 <a368442@uach.mx>
2026-05-21 19:57:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -269,7 +269,7 @@ if submit_button:
     st.markdown("<h3 style='color: #0A2540;'> </h3>", unsafe_allow_html=True)
     
     st.info(
-    "📌 Nota: Si desea modificar los parámetros o condiciones de entrada, "
+    "📌 **Nota:** Si desea modificar los parámetros o condiciones de entrada, "
     "desplácese hacia la parte superior del formulario y ajuste los valores según corresponda. "
     "Los resultados se actualizarán automáticamente con los nuevos datos ingresados."
 )
