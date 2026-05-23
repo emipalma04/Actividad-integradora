0be51095f5324a8f1577219cbccad0776c29a9e8
emipalma04 <a368442@uach.mx>
2026-05-22 22:06:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ with st.form("simulador_form"):
     
     C_MEA = st.number_input("Concentración inicial de la solución de MEA (% en peso)", min_value=0.0, max_value=100.0, value=30.0, step=1.0, format="%.4f")
     
-    st.write("#### :blue[Composición del gas de entrada (Parte inferior - Corriente G₁)]")
+    st.write("#### :blue[Composición del gas de entrada (Parte inferior - Corriente Gn+1)]")
     
     # Inputs numéricos (Se quedan igual)
     y1_CO2_pct = st.number_input("Porcentaje volumétrico de CO₂ (% vol)", min_value=0.0, max_value=100.0, value=15.0, format="%.4f")
