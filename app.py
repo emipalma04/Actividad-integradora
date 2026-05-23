8f72516ea382da7d21c3e2324b58947c9e4b2896
emipalma04 <a368442@uach.mx>
2026-05-22 22:03:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ with st.form("simulador_form"):
             st.warning(st.session_state.mensaje_balance)
     
     st.write("#### :blue[Condiciones de operación y especificaciones de salida]")
-    x2_input = st.number_input("Concentración de $CO_2$ en el líquido de entrada en la parte superior ($x_2$, mol CO₂/mol sol)", min_value=0.0, max_value=0.9999, value=0.0580, format="%.4f")
+    x2_input = st.number_input("Concentración de $CO_2$ en el líquido de entrada en la parte superior ($x_2$, mol CO₂/mol sol)", min_value=0.0, max_value=0.999999999999999, value=0.0580, format="%.4f")
     factor_min = st.number_input("Multiplicador de exceso para la relación real (Factor respecto a $L_{s-min}/G_s$)", min_value=1.0, value=1.2000, step=0.1, format="%.4f")
     y2_CO2_pct = st.number_input("Concentración residual de $CO_2$ deseada en el gas de salida por la parte superior (% vol)", min_value=0.0, max_value=100.0, value=2.0, format="%.4f")
     
